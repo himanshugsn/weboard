79f30213012521bbbe8e49fe6e9d100a4703a1b0
himanshugsn30 <himanshugsn30@gmail.com>
2021-05-06 12:39:15 +0530

    update"

--- chatsite/settings.py
+++ chatsite/settings.py
@@ -125,7 +125,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
